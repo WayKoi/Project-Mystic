4908254e4e804a5382c4e974cfe9fe866493abf7
WayKoi <kyle22221@hotmail.com>
2024-04-30 23:37:36 -0400

    forgot to turn off the deck creator

--- Card Test/Main/Game.cs
+++ Card Test/Main/Game.cs
@@ -15,7 +15,7 @@ namespace Card_Test {
 			test.EditDeck();*/
 
 			// PackCreator.CreatePack();
-			DeckCreator.CreateDeck();
+			// DeckCreator.CreateDeck();
 			// GearCreator.CreateGear();
 
 			/*Writer.WriteGear(TGear.Generate(GearTable.Tango[2], null), "test");
